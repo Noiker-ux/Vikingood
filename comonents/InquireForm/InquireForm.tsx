592e087bfab5269699c4edd20a4fe28cec6e7e82
Noiker-UX <pasha_pasha_201@mail.ru>
2026-05-04 20:19:28 +0300

    customize primaryBtn

--- comonents/InquireForm/InquireForm.tsx
+++ comonents/InquireForm/InquireForm.tsx
@@ -4,7 +4,7 @@ export default function InquireForm() {
   return (
     <form className="w-96">
       <div className="flex justify-between ">
-        <button className="secondaryBtn w-full">Отправить</button>
+        <button className="primaryBtn w-full">Отправить</button>
       </div>
     </form>
   );
