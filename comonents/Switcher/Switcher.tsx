4bd5c194707f8d206261735b636c9fa6495a5eba
Noiker-UX <pasha_pasha_201@mail.ru>
2026-05-05 20:44:56 +0300

    Create basic logic for button Weather

--- comonents/Switcher/Switcher.tsx
+++ comonents/Switcher/Switcher.tsx
@@ -8,12 +8,47 @@ import { Fragment, useState } from "react";
 export default function Switcher() {
   const [weather, setWeather] = useState(getNowWeather());
   const weatherInfo = weatherList.find((item) => item.id === weather);
-
+  const [hover, setHover] = useState(false);
   return (
-    <div className="mt-20 ml-40 flex flex-col">
+    <div className="absolute right-10 bottom-10 mt-20 ml-40 flex flex-col w-fit" onMouseLeave={() => setHover(false)}>
+      <div className={`flex flex-col overflow-hidden  ${hover ? "h-auto " : "h-0"}`}>
+        {weatherList
+          .filter((e) => e.id != weather)
+          .map((weatherItem) => (
+            <Tooltip key={weatherItem.id} delay={0}>
+              <Button
+                isIconOnly
+                className={"rounded-none p-6 bg-[var(--primary-color)] group"}
+                onHoverChange={() => setHover(true)}
+                onClick={() => {
+                  setWeather(weatherItem.id);
+                  setHover(false);
+                }}
+              >
+                <div className="fill-[var(--primary-light-color)] scale-150 group-hover:fill-[var(--background-color)]">
+                  {weatherItem.icon}
+                </div>
+              </Button>
+              <Tooltip.Content
+                className={
+                  "py-2.5 px-6 rounded-none text-md bg-[var(--background-color)] font-bold text-lg text-[var(--primary-color)]"
+                }
+                placement="left"
+                offset={15}
+              >
+                <Tooltip.Arrow />
+                {weatherItem.label}
+              </Tooltip.Content>
+            </Tooltip>
+          ))}
+      </div>
       <Tooltip delay={0}>
-        <Button isIconOnly className={"rounded-none p-6 bg-[var(--background-color)] "}>
-          <div className="text-xl w-7 h-7 fill-[var(--primary-color)]">{weatherInfo?.icon}</div>
+        <Button
+          isIconOnly
+          className={"rounded-none p-6 bg-[var(--background-color)]"}
+          onHoverChange={() => setHover(true)}
+        >
+          <div className="scale-150 fill-[var(--primary-color)]">{weatherInfo?.icon}</div>
         </Button>
         <Tooltip.Content
           className={
@@ -26,25 +61,6 @@ export default function Switcher() {
           {weatherInfo?.label}
         </Tooltip.Content>
       </Tooltip>
-      {weatherList
-        .filter((e) => e.id != weather)
-        .map((weatherItem) => (
-          <Tooltip key={weatherItem.id} delay={0}>
-            <Button isIconOnly className={"rounded-none p-6 bg-[var(--primary-color)] "}>
-              {weatherItem.icon}
-            </Button>
-            <Tooltip.Content
-              className={
-                "py-2.5 px-6 rounded-none text-md bg-[var(--background-color)] font-bold text-lg text-[var(--primary-color)]"
-              }
-              placement="left"
-              offset={15}
-            >
-              <Tooltip.Arrow />
-              {weatherItem.label}
-            </Tooltip.Content>
-          </Tooltip>
-        ))}
     </div>
   );
 }
