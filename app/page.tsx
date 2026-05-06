814d1972f5efa12916b9a0c5959a763e0e882287
Noiker-UX <pasha_pasha_201@mail.ru>
2026-05-06 23:21:16 +0300

    create preview text

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import Switcher from "@/comonents/Switcher/Switcher";
 import InquireForm from "@/comonents/InquireForm/InquireForm";
-
+import StartIcon from "@/public/icons/master.svg";
 export default function Home() {
   return (
-    <div className="">
+    <main className="flex justify-center items-center h-[calc(100vh-3rem)]">
+      <div className="flex flex-col items-center">
+        <StartIcon className="w-4 h-4" />
+        <h1 className="text-white text-9xl">VIKINGOOD</h1>
+        <p className="text-white">Отправляйтесь в настоящий рай для отдыха.</p>
+      </div>
       <Switcher />
-    </div>
+    </main>
   );
 }
--- comonents/Header/Header.tsx
+++ comonents/Header/Header.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import LogoIcon from "@/public/icons/logo-light.svg";
+import SoundIcon from "@/public/icons/sound.svg";
 export default function Header() {
   return (
     <header className=" bg-red-300 flex justify-between items-center px-16 py-3">
-      <button className="">{/* <Image src="/icons/sound-icon.svg" alt="" width={28} height={14} /> */}</button>
+      <button>
+        <SoundIcon className="w-full h-4" />
+      </button>
       <Link href="/">
         <LogoIcon />
       </Link>
